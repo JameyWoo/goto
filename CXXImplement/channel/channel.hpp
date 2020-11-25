0521b69fea28c174b8157b10942a9a48a2d59130
JameyWoo <2622075127wjh@gmail.com>
2020-11-25 10:09:33 +0800

    start CXXImplement project

---
+++ CXXImplement/channel/channel.hpp
@@ -0,0 +1,13 @@
+#if !defined(__CHANNEL_HPP__)
+#define __CHANNEL_HPP__
+
+# include <iostream>
+using namespace std;
+
+class Chan {
+    public:
+    out();
+    int();
+}
+
+#endif // __CHANNEL_HPP__
---
+++ CXXImplement/readme.md
@@ -0,0 +1,12 @@
+# C++ 实现的Go特性
+
+- [ ] channel
+- [ ] WaitGroup
+- [ ] Context
+- [ ] Mutex
+- [ ] RWMutex
+- [ ] Map
+- [ ] sync.Map
+- [ ] 定时器
+- [ ] Goroutine
+
