f3307dc994524970d5bf20e3f70b53fe7d00bd20
JameyWoo <2622075127wjh@gmail.com>
2020-11-26 19:18:29 +0800

    update

--- playground/pkg/testing/example_test.go
+++ playground/pkg/testing/example_test.go
@@ -2,11 +2,19 @@ package _testing
 
 import (
 	"fmt"
+	"testing"
 )
 
 func ExampleAdd() {
 	a := 1
 	b := 2
 	fmt.Println(a + b)
+	// Output: 3
+}
+
+func BenchmarkAdd(b *testing.B) {
+	a := 1
+	c := 2
+	fmt.Println(a + c)
 	// Output: 3
 }
