76bc7bdc966fc6aaf5f6204de3fb7e0e75f7c640
JameyWoo <2622075127wjh@gmail.com>
2020-12-04 17:13:08 +0800

    修改队列, 更简洁的Queue

--- CXXImplement/channel/channel.hpp
+++ CXXImplement/channel/channel.hpp
@@ -2,10 +2,8 @@
 #define MUDUO_BASE_BOUNDEDBLOCKINGQUEUE_H
 
 
-#include <cassert>
-#include <mutex>
 #include <condition_variable>
-#include <vector>
+#include <mutex>
 #include <queue>
 
 
@@ -24,9 +22,6 @@ public:
     void put(T &&x);
     T get();  // <- chan T
 
-    size_t size() const;
-    size_t capacity() const;
-
 private:
     // 锁
     std::mutex mutex_{};
@@ -35,11 +30,8 @@ private:
     std::condition_variable notFull;  // 在 put 中使用
 
 private:
-    size_t capacity_;  // 容量
-    size_t size_;  // 实际的大小
-    std::vector<T> queue_;  // 存储数据
-    int put_idx_;  // 放 下一个数据 的索引
-    int get_idx_;  // 取 当前数据 的索引
+    size_t capacity;  // 容量
+    std::queue<T> Queue;
 
     // 当缓冲为0时存储变量到队列Q中
     std::queue<T> Q;
@@ -53,17 +45,11 @@ Chan<T>::Chan() : Chan(0) {}
 
 template<typename T>
 Chan<T>::Chan(int max_size) {
-    capacity_ = max_size;
-    queue_.resize(capacity_);
-
-    size_ = 0;
-    put_idx_ = 0;
-    get_idx_ = 0;
+    capacity = max_size;
 
     cnt_get = 0;
     cnt_put = 0;
 }
-
 
 template<typename T>
 void Chan<T>::put(const T &x) {
@@ -71,7 +57,7 @@ void Chan<T>::put(const T &x) {
     // unique_lock 具有 lock_guard 的所有功能，而且更为灵活
     std::unique_lock<std::mutex> lck(mutex_);
     // 当缓冲区大小为 0时
-    if (capacity_ == 0) {
+    if (capacity == 0) {
         Q.push(x);
         cnt_put += 1;
         if (cnt_get > 0) {
@@ -87,16 +73,13 @@ void Chan<T>::put(const T &x) {
         return;
     }
 
-    while (size_ == capacity_) {
+    while (Queue.size() == capacity) {
         // 传 引用
         notFull.wait(std::ref(lck));
     }
 
-    assert(put_idx_ < capacity_ && put_idx_ >= 0);
-    queue_[put_idx_] = x;
-    put_idx_++;
-    size_++;
-    put_idx_ = put_idx_ % capacity_;
+    Queue.push(x);
+
     // 告诉消费者可以消费了
     notEmpty.notify_one();
 }
@@ -110,7 +93,7 @@ template<typename T>
 T Chan<T>::get() {
     std::unique_lock<std::mutex> lck(mutex_);
     // 当缓冲区大小为 0时
-    if (capacity_ == 0) {
+    if (capacity == 0) {
         cnt_get += 1;
         if (cnt_put > 0) {
             notFull.notify_one();
@@ -129,28 +112,15 @@ T Chan<T>::get() {
         return tmp;
     }
 
-    while (size_ == 0) {
+    while (Queue.size() == 0) {
         notEmpty.wait(std::ref(lck));
     }
 
-    assert(get_idx_ < capacity_ && get_idx_ >= 0);
-    T val = queue_[get_idx_];
-    get_idx_++;
-    size_--;
-    get_idx_ = get_idx_ % capacity_;
+    T val = Queue.front();
+    Queue.pop();
     notFull.notify_one();
 
     return val;
-}
-
-template<typename T>
-size_t Chan<T>::size() const {
-    return size_;
-}
-
-template<typename T>
-size_t Chan<T>::capacity() const {
-    return capacity_;
 }
 
 #endif
